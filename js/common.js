a15080bf8403397f0748cea9ef11b28c301dbc2f
Dima Os <dimaos@protonmail.com>
2021-06-27 23:41:58 +0300

    Добавляет слайдер

--- js/common.js
+++ js/common.js
@@ -50,3 +50,46 @@
 
   toggle.addEventListener('click', onToggleClick);
 })();
+
+(() => {
+  const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 8,
+    breakpoints: {
+      576: {
+        spaceBetween: 16,
+      },
+      992: {
+        spaceBetween: 24,
+      },
+      1200: {
+        spaceBetween: 32,
+      },
+    }
+  });
+})();
+
+(() => {
+  const form = document.querySelector('.profile-form form');
+
+  const onChangeViewBtnClick = (({target}) => {
+    const btn = target.closest('.profile-form__change-view-btn');
+
+    if (btn) {
+      const wrapper = btn.closest('.profile-form__input-wrapper');
+      const input = wrapper.querySelector('input');
+      const altText = wrapper.querySelector('span');
+
+      if (input.type === 'password') {
+        input.type = 'text';
+        altText.textContent = 'Скрыть пароль';
+      } else if (input.type === 'text') {
+        input.type = 'password';
+        altText.textContent = 'Показать пароль';
+      }
+    }
+  });
+
+  form.addEventListener('click', onChangeViewBtnClick);
+})();
